7f69858bfeda7b60cfe4d6ad9660be9f61b28120
drawinghow <drawinghow@naver.com>
2020-11-22 17:21:05 +0900

    test
    test

---
+++ test/js/common.js
@@ -0,0 +1,15 @@
+function popup_open(popupId){
+    $('#'+popupId).fadeIn(200);
+}
+
+function popup_close(popupId){
+    $('#'+popupId).fadeOut(200);
+}
+
+
+
+$(document).ready(function () {
+
+
+
+}); //$(document).ready
